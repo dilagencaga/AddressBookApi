a573933ba7b9363c7837cfffddae679fd5228912
dila gençağa <dila2002ar@hotmail.com>
2025-11-02 05:48:02 +0300

    cs değiştirildi

--- AddressBookApi/Program.cs
+++ AddressBookApi/Program.cs
@@ -1,11 +1,16 @@
+// Program.cs
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+// KÖK ADRESÝ /swagger'a yönlendir
+app.MapGet("/", () => Results.Redirect("/swagger"));
 
 app.UseHttpsRedirection();
 app.MapControllers();
